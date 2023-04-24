c3ba74d0ac9771f1108a62198977826cc2f24108
usagibravo <usagibravo@gmail.com>
2023-04-24 09:58:09 +0000

    mynews 07 completed

--- mynews/routes/web.php
+++ mynews/routes/web.php
@@ -19,13 +19,13 @@ Route::get('/', function () {
 
 use App\Http\Controllers\Admin\NewsController;
 Route::controller(NewsController::class)->prefix('admin')->group(function() {
-    Route::get('news/create', 'add');
+    Route::get('news/create', 'add')->middleware('auth');
 });
 
 use App\Http\Controllers\Admin\ProfileController;
 Route::controller(ProfileController::class)->prefix('admin')->group(function() {
-    Route::get('profile/create', 'add');
-    Route::get('profile/edit', 'edit');
+    Route::get('profile/create', 'add')->middleware('auth');
+    Route::get('profile/edit', 'edit')->middleware('auth');
 });
 Auth::routes();
 
